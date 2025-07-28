0725b21d29c247aee8ac14f1172451794a56f4b1
kingjinho <jinhoshin319@gmail.com>
2025-07-28 21:00:50 +0900

    finish writing launch & execute function

--- main.c
+++ main.c
@@ -4,6 +4,49 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+char *builtin_str[] = {
+    "cd", "help", "exit"
+};
+
+int (*builtin_func[])(char **) = {
+    &lsh_cd, &lsh_help, &lsh_exit
+};
+
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else if (chdir(args[1]) != 0) {
+        perror("lsh");
+    }
+    return 1;
+}
+
+int lsh_help(char **args) {
+    printf("KingJinho's LSH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("the following are built in:\n");
+
+    for (int i = 0; i < lsh_num_builtins(); i++) {
+        printf("    %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args) {
+    return 0;
+}
+
 //read line
 #define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void) {
@@ -45,6 +88,7 @@ char *lsh_read_line(void) {
 
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
+
 char **lsh_split_line(char *line) {
     int buf_size = LSH_TOK_BUFSIZE;
     int position = 0;
@@ -62,7 +106,7 @@ char **lsh_split_line(char *line) {
         position++;
 
         if (position >= buf_size) {
-            buf_size = (int) realloc(tokens, buf_size * sizeof(char*));
+            buf_size = (int) realloc(tokens, buf_size * sizeof(char *));
 
             if (!tokens) {
                 fprintf(stderr, "lsh: allocation error\n");
@@ -90,14 +134,17 @@ int lsh_launch(char **args) {
         }
         exit(EXIT_FAILURE);
     }
+
+    //fork() error check
     if (pid < 0) {
         //error forking
         perror("lsh");
     } else {
+        //parent process
         do {
+            //wait for the process's state to change
             wpid = waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-
     }
 
     return 1;
@@ -105,7 +152,18 @@ int lsh_launch(char **args) {
 
 // start the process
 int lsh_execute(char **args) {
-    return 0;
+    if (args[0] == NULL) {
+        //empty command
+        return 1;
+    }
+
+    for (int i = 0;i < lsh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
 }
 
 
@@ -121,13 +179,10 @@ static void lsh_loop() {
 
         free(line);
         free(args);
-
     } while (status);
-
 }
 
 int main(int argc, char **argv) {
-
     //init - read config
 
     //interpret - read commands and executes
